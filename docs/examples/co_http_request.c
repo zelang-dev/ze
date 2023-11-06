2ace711a6c3f8c0d9c02f9f7aed3c9013f842b53
Lawrence Stubbs <lstubbs@techno.express>
2023-11-05 19:18:14 -0500

    add deps libressl, libuv-tls, Windows warnings fix
    - add libressl as dependency for libuv http tls handling from https://github.com/libressl/portable/
    - add C app to create self signed cert
    - refactor/merge repo kamcpp/libuv-tls from https://github.com/kamcpp/libuv-tls
    - the test_tls.c default cert is systems hostname, use create_x509 for cert setup
    - remove `gettimeofday()` already in libressl library

--- docs/examples/co_http_request.c
+++ docs/examples/co_http_request.c
@@ -42,4 +42,6 @@ int co_main(int argc, char *argv[]) {
     ZE_ASSERT(is_str_eq("POST", parser->method));
     ZE_ASSERT(is_str_eq("/path", parser->path));
     ZE_ASSERT(is_str_eq("HTTP/1.1", parser->protocol));
+
+    return 0;
 }
